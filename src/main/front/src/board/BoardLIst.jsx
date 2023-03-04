b72009e812101dc8b29ce4465253027c6fb81ad0
lebjent <wnsvy2885@gmail.com>
2023-03-04 18:07:28 +0900

    Complete

--- src/main/front/src/board/BoardLIst.jsx
+++ src/main/front/src/board/BoardLIst.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import axios from 'axios';
 import "../css/common.css";
@@ -37,6 +38,19 @@ const Td= styled.td`
   font-size: 11pt;
 `;
 
+const BoardWriteButton = styled.button`
+  background-color: #005bb5;
+  color: white;
+  width: 120px;
+  height: 50px;
+  border-radius: 10px;
+  border: none;
+
+  &:hover{
+    border: 2px #80d6ff solid;
+  }
+`;
+
 function BoardLIst() {
 
     const [data, setData] = useState([]);
@@ -49,34 +63,49 @@ function BoardLIst() {
   
 
   return (
-    <Main>
-      <Table>
-        <thead>
-        <Tr>
-          <Th>NO.</Th>
-          <Th>제목</Th>
-          <Th>작성자</Th>
-          <Th>조회수</Th>
-          <Th>좋아요수</Th>
-          <Th>작성일</Th>
-        </Tr>
-        </thead>
-        <tbody>
-        {   data && data.map((board,index)=>{
-                return <Tr key={index}>
-                          <Td>{board.bno}</Td>
-                          <Td>{board.title}</Td>
-                          <Td>{board.writer}</Td>
-                          <Td>{board.view_cnt}</Td>
-                          <Td>{board.like_cnt}</Td>
-                          <Td>{board.regdate}</Td>
-                       </Tr>
-            })
-        }
-        </tbody>
-      </Table>
-    </Main>
-  )
+    <>
+      <Main>
+        <h2 className='colorGrey'>React게시판</h2>
+        <Table>
+          <thead>
+          <Tr>
+            <Th>NO.</Th>
+            <Th>제목</Th>
+            <Th>작성자</Th>
+            <Th>조회수</Th>
+            <Th>좋아요수</Th>
+            <Th>작성일</Th>
+          </Tr>
+          </thead>
+          <tbody>
+          { 
+            data && data.length > 0 ? (
+              data && data.map((board,index)=>{
+                  return <Tr key={index}>
+                            <Td>{board.bno}</Td>
+                            <Td>{board.title}</Td>
+                            <Td>{board.writer}</Td>
+                            <Td>{board.view_cnt}</Td>
+                            <Td>{board.like_cnt}</Td>
+                            <Td>{board.regdate}</Td>
+                        </Tr>
+              })
+            )  : (
+              <Tr>
+                <Td colSpan={6} rowSpan={6}>데이터가 없습니다.</Td>
+              </Tr>
+            )
+          }  
+          </tbody>
+        </Table>
+        <Link>
+        <div className='w90p tar mt30'>
+          <BoardWriteButton>게시글 작성</BoardWriteButton>
+        </div>  
+        </Link>
+      </Main>
+    </>
+  ) 
 }
 
 export default BoardLIst
